2fa1724ca5c9c09b8d11e09337fd8c4a4e5378d9
Paul Jovanovic <paulrealsane@gmail.com>
2014-05-22 16:40:02 -0500

    Can quote sources again

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,7 @@ class QuotesController < ApplicationController
     quote = Quote.new(quote_params)
 
     if quote.save
-      render json: { quote: { id: quote.id, citable_type: quote.citable_type, citable_id: quote.citable_id, text: quote.text } }
+      render json: { id: quote.id, citable_type: quote.citable_type, citable_id: quote.citable_id, text: quote.text }
     else
       render json: { errors: quote.errors }, status: :unprocessable_entity
     end
