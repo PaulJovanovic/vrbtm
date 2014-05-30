d7b61e54eea1d5726a231c6c18efc472bfd53b2b
Paul Jovanovic <paulrealsane@gmail.com>
2014-05-30 15:36:32 -0700

    Attempt to speed up homes controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = current_user.feed.paginate(page: params[:page], per_page: 10).order("created_at desc")
+    @posts = current_user.feed.includes(:quote).paginate(page: params[:page], per_page: 10).order("created_at desc")
     @trending_sources = Source.joins(:posts).where("posts.created_at > ?", 1.day.ago).group(:citable_id, :name).order('count_id desc').limit(5).count('id')
   end
 end
