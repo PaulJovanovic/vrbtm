c3e92cfd248fe345aef8d91d523275594ffb4987
Paul Jovanovic <paulrealsane@gmail.com>
2014-05-30 15:54:37 -0700

    Verified accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530164220) do
+ActiveRecord::Schema.define(version: 20140530224542) do
 
   create_table "assets", force: true do |t|
     t.string   "type"
@@ -92,12 +92,12 @@ ActiveRecord::Schema.define(version: 20140530164220) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20140530164220) do
     t.datetime "updated_at"
     t.string   "favorite_quote"
     t.string   "favorite_quote_author"
+    t.boolean  "verified",               default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
