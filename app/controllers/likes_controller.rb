f7ddf7060776b96f9f218aa62a20cf9b8f0594de
Paul Jovanovic <paulrealsane@gmail.com>
2014-06-09 17:08:53 -0700

    Revert likes controller changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,15 +4,9 @@ class LikesController < ApplicationController
     like = Like.new(like_params)
 
     if like.save
-      respond_to do |format|
-        format.html redirect_to send("#{like_params[:likable_type].downcase}_path", like_params[:likable_id])
-        format.json render json: { id: like.id }
-      end
+      render json: { id: like.id }
     else
-      respond_to do |format|
-        format.html redirect_to send("#{like_params[:likable_type].downcase}_path", like_params[:likable_id])
-        format.json render json: { errors: like.errors }
-      end
+      render json: { errors: like.errors }
     end
   end
 
