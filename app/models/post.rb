cb418a10d68603b7a982dee81b082ee6497d03af
Paul Jovanovic <paulrealsane@gmail.com>
2014-07-02 12:34:55 -0700

    Quoting users

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,7 @@ class Post < ActiveRecord::Base
   validates :user, :quote, presence: true
 
   after_create :transfer_tags_to_quote
+  after_create :notify_user, if: :notify_user?
 
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
@@ -45,5 +46,15 @@ class Post < ActiveRecord::Base
   def transfer_tags_to_quote
     quote.tags << tags
     quote.save
+  end
+
+  private
+
+  def notify_user?
+    quote.citable_type = "User" && user_id != quote.citable_id && Notification.where(notifiable_type: "Post", notifiable_id: id, from_user: user, to_user: quote.citable, action: "quoted").count == 0
+  end
+
+  def notify_user
+    Notification.create(notifiable_type: "Post", notifiable_id: id, from_user: user, to_user: quote.citable, subject: user.name, action: "quoted", text: "you in a #{notification_text}")
   end
 end
