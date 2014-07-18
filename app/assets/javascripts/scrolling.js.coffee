a7d482e9868e7d3631173d8fcb264980fc13a02d
Paul Jovanovic <paulrealsane@gmail.com>
2014-07-18 14:56:08 -0500

    Infinite scroll

---
+++ app/assets/javascripts/scrolling.js.coffee
@@ -0,0 +1,12 @@
+$(document).ready ->
+  $(".js-infinite-scroll").each ->
+    $(@).infinitescroll
+      navSelector  : ".pagination"
+      nextSelector : ".pagination a:last"
+      # // selector for the NEXT link (to page 2)
+      itemSelector : ".js-post"
+      loadingText  : "Loading new posts...",
+      # // selector for all items you'll retrieve
+
+    , (arrayOfNewElems) ->
+      $("#infscr-loading").remove()
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:privacy]
   def index
-    @posts = current_user.feed.includes(:quote).paginate(page: params[:page], per_page: 10).order("created_at desc")
+    @posts = current_user.feed.includes(:quote).paginate(page: params[:page], per_page: 15).order("created_at desc")
     @trending_sources = Source.joins(:posts).where("posts.created_at > ?", 1.day.ago).group(:citable_id, :name).order('count_id desc').limit(5).count('id')
     @suggested_users = User.where('id not in (?)', current_user.relationships.map(&:followed_id).push(current_user.id)).order("posts_count DESC").limit(5)
   end
